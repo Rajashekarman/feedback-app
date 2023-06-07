730aca74c5804140bc270dcf2227a582438ee720
Rajashekarm <shekarraja8899@gmail.com>
2023-06-07 16:57:16 +0000

    solution

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -1,17 +1,29 @@
 import {Component} from 'react'
 
 class Feedback extends Component {
+  thanks = () => {
+    return (
+        <img src = {loveEmojiUrl}/>
+    <h1>Thank You</h1>
+    )
+  }
+
   render() {
     const {resources} = this.props
-    const {name, imageUrl} = resources
+    const {emojis, loveEmojiUrl} = resources
+    const {name} = emojis
     return (
       <div>
-        <h1>How satisfied are you with our customer support perfomance </h1>
+        <h1>How satisfied are you with our customer support performance </h1>
         <ul>
-          {resources.map(each => (
-            <li>
-              <p>{each.name}</p>
-              <img src={each.imageUrl} alt={each.name} />
+          {resources.emojis.map(emoji => (
+            <li key={emoji.id}>
+              <p>{emoji.name}</p>
+              <img
+                src={emoji.imageUrl}
+                alt={emoji.name}
+                onChange={this.thanks}
+              />
             </li>
           ))}
         </ul>
